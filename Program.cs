d6ffbe22608e5f76b99a6fd244da1a39e137124e
AndreyGrabovenko <grabovenko.andrei@mail.ru>
2022-03-31 16:04:11 +0300

    finished program

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+
+string[] array = {"hello","2","world",":-)"};
+
+string a = string.Empty;
+string[] array1 = new string [array.Length];
+for (int i = 0; i < array.Length ; i++)
+{
+        System.Console.Write(array[i] + " ");
+        a = array[i];
+        if(a.Length <= 3)
+        array1[i] = a;
+}
+System.Console.WriteLine();
+foreach(string B in array1)
+{
+    System.Console.Write(B);
+}
